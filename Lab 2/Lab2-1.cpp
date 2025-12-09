7c2cc88dbfbf3533fc6e5e6a69f5c5aea6d0556f
htunzayarlin001 <684244111@mail.pbru.ac.th>
2025-12-09 15:32:25 +0700

    Add files via upload

---
+++ Lab 2/Lab2-1.cpp
@@ -0,0 +1,18 @@
+#include <iostream> // Including the Input/Output Stream Library
+#include <string>   // Including the String Library
+using namespace std;
+
+int main() {
+    char a[6] = {'a', 'b', 'c', 'd', 'e', 'f'}; // Declare and initialize another character array
+    char b[6] = {'A', 'B', 'C', 'D', 'E', 'F'}; // Declare and initialize a character array
+    char temp;
+    string mess = "AbbeF"; // Declare and initialize a string variable
+    for (int i = 0; i < mess.length(); i++) {
+       temp=mess[i];
+       if ((int)temp>90) {
+        cout<<b[(int)temp - (int)'a'];
+       } else {
+        cout<<a[(int)temp - (int)'A'];
+    }
+}
+}
